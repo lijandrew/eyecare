ea5dcc86c01677fe19e42a85632008433277a1a2
Andrew <39812702+lijandrew@users.noreply.github.com>
2020-09-09 23:18:10 -0400

    mobile nav animations almost done

--- app.js
+++ app.js
@@ -1,24 +1,33 @@
 let body = document.querySelector("body");
 
+function setupMobileNav() {
+  let burger = document.querySelector("#burger");
+  let nav = document.querySelector("nav");
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("mobile-nav-open");
+  })
+}
 
 function setupHome() {
   let readMore = document.querySelector("#read-more");
+  let readMoreSpan = readMore.querySelector("span");
   let span1 = document.querySelector("section#about p span:nth-child(1)");
   let span2 = document.querySelector("section#about p span:nth-child(2)");
   readMore.addEventListener("click", () => {
     span1.classList.toggle("shown");
     span2.classList.toggle("shown");
+    readMore.classList.toggle("shown");
     if (span2.classList.contains("shown")) {
-      readMore.textContent = "read less";
+      readMoreSpan.textContent = "read less";
     } else {
-      readMore.textContent = "read more";
+      readMoreSpan.textContent = "read more";
     }
   });
 }
 
-
 function main() {
   window.onload = () => {
+    setupMobileNav();
     setupHome();
     body.style.opacity = 1;
   };
